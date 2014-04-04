7eb17b17c0db47613a51b5c78202897db349ab7a
James C Russell <james@101ideas.cz>
2014-04-04 22:38:50 +0200

    XYZ

--- app.js
+++ app.js
@@ -7,42 +7,7 @@ TODO: Retry after going online.
 app = angular.module('app', ['ui.bootstrap', 'ngRoute', 'ng', 'ngSanitize']);
 
 app.controller('HabitsCtrl', function ($scope) {
-  $scope.groups = [
-    {title: 'Trigger: Alarm Goes Off', focus: {from: 4, to: 9}, habits: [
-      {title: 'Action as soon as the alarm goes off'},
-      {title: 'First thing in the morning: drink water'},
-      {title: 'Commit to be happy'},
-      {title: 'Make an entry to the Five Minute Journal'},
-      {title: 'Watch a motivational video', link: 'http://www.youtube.com/watch?v=wfcro5iM5vw'},
-      {title: 'Implement your new identity'},
-    ]},
-    {title: 'Trigger: Getting Up From The Bed', focus: {from: 7, to: 9}, habits: [
-      {title: 'Clean teeth'},
-      {title: 'Do 10 push-ups or pull-ups'},
-      {title: 'Make a power pose'},
-    ]},
-    {title: 'Trigger: Sitting Down To My Desk', focus: {from: 9, to: 11}, habits: [
-      {title: 'Go through 3 Evernote notes'},
-      {title: 'Write a blog post (or a part of it, if it’s too long)'},
-      {title: 'Do the most important task before 12'}
-    ]},
-    {title: 'Trigger: Lunch', focus: {from: 13, to: 16}, habits: [
-      {title: 'Take a nap or listen to one Headspace episode'}
-    ]},
-    {title: 'Trigger: I Finished Work For The Day', focus: {from: 19, to: 20}, habits: [
-      {title: 'Close all tabs in Chrome'}
-    ]},
-    {title: 'Trigger: Dinner', focus: {from: 20, to: 22}, habits: [
-      {title: 'Merge current tasks to the task log'},
-      {title: 'Reflection'},
-      {title: 'Prepare tasks for tomorrow'}
-    ]},
-    // GET /habits/2014-01-31
-    //
-    // {title: 'Daily Special', focus: {from: 10, to: 12}, habits: [
-    //   {title: 'Go swimming', weekday: 3},
-    // ]}
-  ];
+  $scope.groups = GROUPS;
 
   $scope.groupFinished = function (group) {
     return this.group.habits.every(function (habit) {
@@ -63,23 +28,7 @@ app.controller('HabitsCtrl', function ($scope) {
 app.controller('TasksCtrl', function ($scope) {
   $scope.today = new Date().getTime();
 
-  $scope.tasks = {
-    fixedTime: [
-      {time: '5:30 PM', title: 'Test Skype call', tags: ['online']}
-    ],
-    important: [
-      {title: 'Leadsorama <a href="https://github.com/leadsorama/web-client/issues/22">#22</a>', tags: []},
-      {title: 'Captureproof', tags: []},
-      {title: 'Today', tags: []}
-    ],
-    urgencies: [
-      {title: 'One', tags: []},
-      {title: 'Two', tags: []},
-      {title: 'Three', tags: []},
-      {title: 'Four', tags: []},
-      {title: 'Five', tags: []}
-    ]
-  };
+  $scope.tasks = TASKS;
 
   $scope.showEditButton = function (event) {
     var getContentEditableElement = function (element) {
@@ -100,6 +49,7 @@ app.controller('TomorrowCtrl', function ($scope) {
   //
 });
 
+// Documentation: http://docs.angularjs.org/guide/directive
 // ng-mouseover="showEditButton($event)" ng-blur="makeNotEditable()"
 app.directive('toggleEditable', function ($document) {
   return function ($scope, $element, $attrs) {
