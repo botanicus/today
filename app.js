fd0266e4d54f1b95a4c657a0824507936b2da025
James C Russell <james@101ideas.cz>
2017-01-15 01:43:33 -0300

    Automated commit from 15/01/2017.

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-/*
-
-TODO: Retry after going online.
-
- */
-
-app = angular.module('app', ['ui.bootstrap', 'ngRoute', 'ng', 'ngSanitize']);
+var app = angular.module('app', ['ui.bootstrap', 'ngRoute', 'ng', 'ngSanitize', 'directives', 'menu']);
 
 app.controller('HabitsCtrl', function ($scope) {
   $scope.groups = GROUPS;
@@ -41,63 +35,8 @@ app.controller('TasksCtrl', function ($scope) {
 
     var contentEditableElement = getContentEditableElement(event.toElement);
   };
-
-  window.$scope = $scope;
 });
 
 app.controller('TomorrowCtrl', function ($scope) {
   //
-});
-
-// Documentation: http://docs.angularjs.org/guide/directive
-// ng-mouseover="showEditButton($event)" ng-blur="makeNotEditable()"
-app.directive('toggleEditable', function ($document) {
-  return function ($scope, $element, $attrs) {
-    var element = $element[0];
-    var menu = element.getElementsByClassName('menu')[0];
-    var editLink = element.getElementsByClassName('make-editable')[0];
-    var addNewLink = element.getElementsByClassName('add-new-task')[0];
-    var editableArea = element.getElementsByClassName('editable-area')[0];
-    var displayArea  = element.getElementsByClassName('display-area')[0];
-    var editable = element.getElementsByClassName('editable')[0];
-
-    // Defaults.
-    menu.style.display = 'none';
-    editableArea.style.display = 'none';
-
-    // Make it editable.
-    angular.element(editLink).on('click', function (event) {
-      displayArea.style.display = 'none';
-      editableArea.style.display = 'inline'
-
-      editable.focus();
-      document.execCommand('selectAll', false, null);
-    });
-
-    // Finish editing.
-    var finishEditing = function () {
-      editableArea.style.display = 'none';
-      displayArea.style.display = 'inline';
-    };
-
-    angular.element(editableArea).on('focusout', finishEditing);
-
-    // Show the edit editLink.
-    $element.on('mouseenter', function (event) {
-      menu.style.display = 'inline';
-    });
-
-    // Hide the edit editLink.
-    $element.on('mouseleave', function (event) {
-      menu.style.display = 'none';
-    });
-
-    // Ignore Enter key.
-    $element.on('keydown', function (event) {
-      if (event.keyCode == 13) {
-        finishEditing();
-        event.preventDefault();
-      };
-    });
-  };
 });
